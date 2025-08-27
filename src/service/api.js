6b36bebaf9beeb0e60ad404e30e9a37f795e3ec5
ShukurullaHackaton1 <sayidabdullatursinbayev205@gmail.com>
2025-08-27 11:27:15 +0500

    start

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5555/api";
+axios.defaults.baseURL = "https://speech.kerek.uz/api";
 
 axios.interceptors.request.use((option) => {
   const token = localStorage.getItem("speech-token")
